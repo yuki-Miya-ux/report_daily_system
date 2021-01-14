1239a425f23798b773c923f36b73fa5ea5d3861d
yuki.Miya <yuki.miya0330@gmail.com>
2021-01-14 19:55:06 +0900

    Add models_Follow

---
+++ src/models/Follow.java
@@ -0,0 +1,59 @@
+package models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Table(name = "follows")
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllFollows",
+            query = "SELECT f FROM Follow AS f ORDER BY f.id DESC"
+            ),
+    @NamedQuery(
+            name = "getFollowsCount",
+            query = "SELECT COUNT(f) FROM Follow AS f"
+            ),
+})
+
+
+@Entity
+public class Follow {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumns({
+        @JoinColumn(name = "user_id",nullable = false),
+        @JoinColumn(name = "follow_id", nullable = false),
+    })
+    private Employee employee;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+
+}
